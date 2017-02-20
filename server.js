f829a4ef4658c01e4cda72826c3a3e759721f67c
RawatP <singhrawatpradeep70@gmail.com>
2017-02-19 21:39:07 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one',function(req,res){
+    res.send('artical-one response will be serverd here');
+});
+app.get('/artical-two',function(req,res){
+    res.send('artical-two response will be serverd here');
+});
+app.get('/artical-three',function(req,res){
+    res.send('artical-three response will be serverd here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
