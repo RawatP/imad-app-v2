e1d5403e5726321806bd5d068f6eff9e0a01b1bc
RawatP <singhrawatpradeep70@gmail.com>
2017-02-21 05:00:31 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,15 +34,56 @@ var artical={
     date:'feb 20 2017',
     content: `<p>
                          This is the content of my Third artical.This is the content of my Third artical.This is the content of my Third artical.This is the content of my Third artical.This is the content of my Third artical.
-                        </p> `},
-}
+                        </p> `
+    
+},
+
 };
 
+
+function createTemplate(data){
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+
+var htmlTemplate=`
+
+<html>
+    <head>
+        <title>
+            ${title }
+            </title>
+            <meta name="viewport" content="width=device-width, inital-scale=1">
+             <link href="/ui/style.css" rel="stylesheet" />
+                </head>
+        <body>
+            <div class="container">
+                    <div>
+                        <a  href="/">Home</a>
+                    </div>
+                    <hr/>
+                     <h3>
+                     ${heading}
+                      </h3>
+                        <div>
+                            ${date}
+                        </div>
+                        <div>
+                        ${content}    
+                        </div>
+          </div>
+        </body>
+</html>
+
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articalName',function(req,res){
+app.get('/:articalName',function(req,res){
     var articalName=req.param.articalName;
   res.send(createTemplate(articala[articalName]));
 });
