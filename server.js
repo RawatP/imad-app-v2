5d080f5cbe2afdfad96e24cf592f062cd4e0fffd
RawatP <singhrawatpradeep70@gmail.com>
2017-02-21 04:41:41 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articalOne= {
+var artical={   
+ `artical-one`: {
     title:' Artical one | RawatP',
     heading:'Artical One',
     date:'feb 20 2017',
@@ -13,58 +14,33 @@ var articalOne= {
                          This is the content of my first artical.This is the content of my first artical.This is the content of my first artical.This is the content of my first artical.This is the content of my first artical.
                         </p> `
 
+},
+`artical-two':{ 
+    
+
+    title:' Artical Two | RawatP',
+    heading:'Artical Two',
+    date:'feb 20 2017',
+    content: `<p>
+                         This is the content of my Second artical.This is the content of my Second artical.This is the content of my Second artical.This is the content of my Second artical.This is the content of my Second artical.
+                        </p> `},
+`artical-three`:{
+    title:' Artical Three | RawatP',
+    heading:'Artical Three',
+    date:'feb 20 2017',
+    content: `<p>
+                         This is the content of my Third artical.This is the content of my Third artical.This is the content of my Third artical.This is the content of my Third artical.This is the content of my Third artical.
+                        </p> `},
+}
 };
 
-function createTemplate(data){
-var title=data.title;
-var date=data.date;
-var heading=data.heading;
-var content=data.content;
-
-var htmlTemplate=`
-
-<html>
-    <head>
-        <title>
-            ${title }
-            </title>
-            <meta name="viewport" content="width=device-width, inital-scale=1">
-             <link href="/ui/style.css" rel="stylesheet" />
-                </head>
-        <body>
-            <div class="container">
-                    <div>
-                        <a  href="/">Home</a>
-                    </div>
-                    <hr/>
-                     <h3>
-                     ${heading}
-                      </h3>
-                        <div>
-                            ${date}
-                        </div>
-                        <div>
-                        ${content}    
-                        </div>
-          </div>
-        </body>
-</html>
-
-`;
-return htmlTemplate;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artical-one',function(req,res){
-  res.send(createTemplate(articalOne));
-});
-app.get('/artical-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
-});
-app.get('/artical-three',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
+app.get('/articalName',function(req,res){
+    var articalName=req.param.articalName;
+  res.send(createTemplate(articala[articalName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
