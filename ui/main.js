1fad918dc47709f9924e5e41e1e5bb7271e88f4d
RawatP <singhrawatpradeep70@gmail.com>
2017-03-08 20:32:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,11 @@ button.onclick=function()
         {
             if(request.status===200)
             {
-                 var counter=request.responsesText;
+                 var counter=request.responseText;
                  var span=document.getElementById('count');
     span.innerHTML=counter.toString();
             }
-        }
+        }   
     };
     request.open('GET','http://rawatp.imad.hasura-app.io/counter',true);
     request.send(null);
