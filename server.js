bc4e0d4119de1b6ed42b837b8648053155568c05
RawatP <singhrawatpradeep70@gmail.com>
2017-03-12 14:54:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,86 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={ 
+
+'article-one':{
+    titl:'Article one|Neo',
+    heading:'Article one',
+    date:'sep 5, 2016',
+    content:`
+    <p>
+                This is the content for first article.This is the content for first article..This is the content for first article.This is the content for first article.
+            </p>
+            <p>
+                This is the content for first article.This is the content for first article..This is the content for first article.This is the content for first article.
+    </p>`
+      
+},
+'article-two':{
+    titl:'Article Two|Neo',
+    heading:'Article Two',
+    date:'sep 5, 2016',
+    content:`
+    <p>
+                This is the content for Second article.This is the content for Second article..This is the content for Second article.This is the content for Second article.
+            </p>
+            <p>
+                This is the content for Second article.This is the content for Second article..This is the content for Second article.This is the content for Second article.
+    </p>`
+      
+},
+'article-three':{
+    titl:'Article Three|Neo',
+    heading:'Article Three',
+    date:'sep 5, 2016',
+    content:`
+    <p>
+                This is the content for Third article.This is the content for Third article..This is the content for Third article.This is the content for Third article.
+            </p>
+            <p>
+                This is the content for Third article.This is the content for Third article..This is the content for Third article.This is the content for Third article.
+    </p>`
+      
+}
+};
+function createTemplate(data){
+    var titl=data.titl;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=`
+
+<html>
+    <head>
+        <title>
+            ${titl}
+        </title>    
+        <meta name="viewport" content="width=device-width, initial-scale-1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>   
+        <hr/>//horizontal line
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+    </div>
+    </body>    
+</html>
+`;
+
+return htmlTemplate;
+}
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
