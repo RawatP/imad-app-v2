12a9755636fca9ca428412c51fdc7f31af343898
RawatP <singhrawatpradeep70@gmail.com>
2017-03-13 18:51:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,7 @@ button.onclick=function()
             if(request.status===200)
             {
                  var counter=request.responseText;
+                 names=JSON.parse(names);
                  var span=document.getElementById('count');
     span.innerHTML=counter.toString();
             }
@@ -24,9 +25,17 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function()
-{//make a request to the server and send name
-//capture a list of names and render it as a list
-  var names=['name1', 'name2','name3','name4'];  
+
+{
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+    {
+        var names=request.responseText;
+        name=JSON.parse(names);
+        
   var list='';
   for(var i=0;i<names.length;i++)
     {
@@ -35,4 +44,10 @@ submit.onclick=function()
     
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+    }
+            
+        }
+};
+request.open('GET','http://rawatp.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
