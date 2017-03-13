89ae98839f26fd499832a2f1c3929749c7b8ed15
RawatP <singhrawatpradeep70@gmail.com>
2017-03-13 20:29:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick=function()
             if(request.status===200)
     {
         var names=request.responseText;
-        name=JSON.parse(names);
+        names=JSON.parse(names);
         
   var list='';
   for(var i=0;i<names.length;i++)
@@ -50,6 +50,6 @@ submit.onclick=function()
 
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-request.open('GET','http://rawatp.imad.hasura-app.io/counter'+ name ,true);
+request.open('GET','http://rawatp.imad.hasura-app.io/submit-name?name='+ name ,true);
     request.send(null);
 };
